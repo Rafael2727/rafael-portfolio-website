898872c6dcdfb8040f618cce2ae2f682c751f268
Rafael2727 <105006222+Rafael2727@users.noreply.github.com>
2026-09-04 00:09:13 +0800

    Fix base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rafael-portfolio-website/', // Exactly matches your GitHub repository name
+  base: '/rafael-portfolio-website/', 
 });
